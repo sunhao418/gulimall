f1d5fa0e4422c60ae87e9f0d5598254ab6b32ef6
sunhaojc@163.com <sunhaojc@163.com>
2022-07-06 08:42:27 +0800

    f

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -33,8 +33,7 @@ class GulimallProductApplicationTests {
 //        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
 //        String endpoint = "oss-cn-hangzhou.aliyuncs.com";
 //        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//        String accessKeyId = "LTAI5tJkeWcPQAv4dQyAXfjA";
-//        String accessKeySecret = "JJ8GZNhzkE1yn6bZVQrSk0Bat9pvFi";
+
 //
 //        // 创建OSSClient实例。
 //        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
